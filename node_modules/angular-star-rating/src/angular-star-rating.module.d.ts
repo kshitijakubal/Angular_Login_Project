88b7719af35874a3fa2395ad6ee1739a2c659ee7
kshitijakubal <kshitijakubal14071997@gmail.com>
2020-10-30 01:33:32 +0530

    Final commit

---
+++ node_modules/angular-star-rating/src/angular-star-rating.module.d.ts
@@ -0,0 +1,13 @@
+import { ModuleWithProviders } from '@angular/core';
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './components/star-rating/star-rating.component';
+import * as ɵngcc2 from './components/star-rating-control/star-rating-control.component';
+import * as ɵngcc3 from '@angular/common';
+export declare class StarRatingModule {
+    static forRoot(): ModuleWithProviders<StarRatingModule>;
+    static forChild(): ModuleWithProviders<StarRatingModule>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<StarRatingModule, [typeof ɵngcc1.StarRatingComponent, typeof ɵngcc2.StarRatingControlComponent], [typeof ɵngcc3.CommonModule], [typeof ɵngcc1.StarRatingComponent, typeof ɵngcc2.StarRatingControlComponent]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDef<StarRatingModule>;
+}
+
+//# sourceMappingURL=angular-star-rating.module.d.ts.map
